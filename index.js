fef35dc400b2201a1a1ab3d75729c21a7c2abee3
EvandroGibicoski <evandrogibicoski@outlook.com>
2019-10-09 10:48:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+let numberOfRequests = 0;
+const projects = [];
+
+// Middleware => project exists
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+
+  return next();
+}
+
+// Middleware => number projects log request.
+
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`requests numbers: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+// Projects
+// get method
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+// Post method
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+// put method
+server.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+// delete method
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+//Tasks
+// post method
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3000);
